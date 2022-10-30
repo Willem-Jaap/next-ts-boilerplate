9c64f55d39cb8a892a0b4cfa755f21d95d18dab9
Willem-Jaap Klepper <wklepper@emerit.nl>
2022-10-30 16:00:30 +0100

    Add SCSS & files/folder structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 // Package imports
 import React from 'react';
 
+// Style imports
+import '@styles/global.scss';
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html>
